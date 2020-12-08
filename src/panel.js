189304a68b972d46d14b52c070ba9e09a631784f
Dj0ulo <julien.launois@student.uclouvain.be>
2020-12-08 17:11:27 +0100

    fixed engines

--- src/panel.js
+++ src/panel.js
@@ -17,201 +17,204 @@ else if (siteFound.search(".google.") != -1)
 else if (siteFound.search(".yahoo.") != -1)
     engine = Yahoo
 
-const searchString = document.querySelector(Engines[engine].searchBox).value
-if(!searchString)
-    console.warn("No search string detected");
-
-console.log(`OptiSearch - ${engine} : "${searchString}"`)
-
 //Not await !!
-loadSettings().then(save => {
-    //Tools
-    if (save['bangs']) {
-        const regexp = /[?|&]q=((%21|!)[^&]*)/
-        const reg = window.location.href.match(regexp)
-        if (reg) {
-            console.log(reg['1']);
-            window.location.href = "https://duckduckgo.com/?q=" + reg['1'];
-        }
-    }
+loadEngines().then(engines => {
 
-    if (save['calculator']) {
-        if (window.location.href.search(/[?|&]q=calculator(&?|$)/) != -1) {
-            const iframe = document.createElement("iframe");
-            iframe.id = "opticalculator"
-            iframe.className = PANEL_CLASS;
-            iframe.src = "https://www.desmos.com/scientific";
-            appendPanel(iframe);
-        }
-    }
+    const searchString = document.querySelector(engines[engine].searchBox).value
+    if (!searchString)
+        console.warn("No search string detected");
 
-    const rep = isMathExpr(searchString);
-    if (rep) {
-        if (rep.vars.length > 0) {
-            if (save['plot']) {
-                let fun = {
-                    expr: rep.expr,
-                    vars: rep.vars
-                }
-                let graph = document.createElement("div");
-                graph.id = "optiplot";
-                graph.className = PANEL_CLASS;
-                appendPanel(graph);
-                plotFun(fun, "optiplot");
+    console.log(`OptiSearch - ${engine} : "${searchString}"`)
+
+    //Not await !!
+    loadSettings().then(save => {
+        //Tools
+        if (save['bangs']) {
+            const regexp = /[?|&]q=((%21|!)[^&]*)/
+            const reg = window.location.href.match(regexp)
+            if (reg) {
+                console.log(reg['1']);
+                window.location.href = "https://duckduckgo.com/?q=" + reg['1'];
             }
-        } else if (save['calculator']
-            && (typeof rep.answer == 'number' || typeof rep.answer == 'boolean' || rep.answer.entries)) {
-            let expr = document.createElement("div");
-            expr.id = "optiexpr";
-            expr.className = PANEL_CLASS;
-
-            let str = "$" + math.parse(rep.expr).toTex() + "~";
-            let answer = rep.answer;
-            if (typeof answer == 'number') {
-                str += "=~" + answer;
-            }
-            else if (typeof answer == 'boolean') {
-                str += ":~" + answer;
-            } else if (rep.answer.entries) {
-                answer = answer.entries[0];
-                str += "=~" + answer;
-            }
-            str += "$";
-            expr.innerHTML = str;
-
-            runMathJax(expr);
-            appendPanel(expr).querySelector("#optiexpr").appendChild(createCopyButton(answer.toString()));
         }
-    }
 
-    //Sites
-    var port = chrome.runtime.connect();
-
-    const results = document.querySelectorAll(Engines[engine].resultRow);
-    if(results.length === 0)
-        console.warn('No result detected')
-
-    for(let r of results){
-        const link = r.querySelector("a").href;
-        const found = Object.keys(Sites).find(site => save[site] && link.search(Sites[site].link) != -1)
-        if(found){  
-            port.postMessage({
-                engine: engine,
-                link: link,
-                site: found
-            });
-            break;
+        if (save['calculator']) {
+            if (window.location.href.search(/[?|&]q=calculator(&?|$)/) != -1) {
+                const iframe = document.createElement("iframe");
+                iframe.id = "opticalculator"
+                iframe.className = PANEL_CLASS;
+                iframe.src = "https://www.desmos.com/scientific";
+                appendPanel(iframe);
+            }
         }
-    }
 
-
-    //set panel
-    function appendPanel(panel) {
-        let knowledgePanel = document.createElement("div");
-        knowledgePanel.className = "optisearchbox";
-        if (engine == Ecosia)
-            knowledgePanel.style.marginTop = "20px";
-        knowledgePanel.style.marginBottom = "20px";
-
-        knowledgePanel.appendChild(panel);
-
-        const rightColumn = document.querySelector(Engines[engine].rightColumn)
-        if(!rightColumn)
-            console.warn('No right column detected')
-        else
-            rightColumn.appendChild(knowledgePanel);
-        return knowledgePanel;
-    }
-    port.onMessage.addListener(msg => {
-        var panel;
-        let icon;
-
-        // console.log(msg)
-
-        for (const site in Sites) {
-            if (Sites.hasOwnProperty(site)) {
-                const p = Sites[site];
-                if (msg.site == site) {
-                    icon = p.icon;
-                    panel = p.set(msg);
-                    if (site == 'stackexchange') {
-                        getChildrenTex(panel.body);
+        const rep = isMathExpr(searchString);
+        if (rep) {
+            if (rep.vars.length > 0) {
+                if (save['plot']) {
+                    let fun = {
+                        expr: rep.expr,
+                        vars: rep.vars
                     }
-                    break;
+                    let graph = document.createElement("div");
+                    graph.id = "optiplot";
+                    graph.className = PANEL_CLASS;
+                    appendPanel(graph);
+                    plotFun(fun, "optiplot");
                 }
+            } else if (save['calculator']
+                && (typeof rep.answer == 'number' || typeof rep.answer == 'boolean' || rep.answer.entries)) {
+                let expr = document.createElement("div");
+                expr.id = "optiexpr";
+                expr.className = PANEL_CLASS;
+
+                let str = "$" + math.parse(rep.expr).toTex() + "~";
+                let answer = rep.answer;
+                if (typeof answer == 'number') {
+                    str += "=~" + answer;
+                }
+                else if (typeof answer == 'boolean') {
+                    str += ":~" + answer;
+                } else if (rep.answer.entries) {
+                    answer = answer.entries[0];
+                    str += "=~" + answer;
+                }
+                str += "$";
+                expr.innerHTML = str;
+
+                runMathJax(expr);
+                appendPanel(expr).querySelector("#optiexpr").appendChild(createCopyButton(answer.toString()));
             }
         }
-        if (!panel)
-            return;
 
+        //Sites
+        var port = chrome.runtime.connect();
 
-        let host = msg.link.match("https?://[^/]+")[0];
+        const results = document.querySelectorAll(engines[engine].resultRow);
+        if (results.length === 0)
+            console.warn('No result detected')
 
-        var sidePanel = document.createElement("div");
-        sidePanel.className = PANEL_CLASS;
-
-        const watermark = document.createElement("div");
-        watermark.className = "watermark";
-        watermark.textContent = 'OptiSearch'
-        sidePanel.appendChild(watermark)
-
-        var headPanel = document.createElement("div");
-        headPanel.className = "optiheader";
-
-        msg.title = msg.title.replace(/<(\w*)>/g, '&lt;$1&gt;');
-
-        var link = "<a href='" + msg.link + "'><div class='title'>" + msg.title + "</div>";
-        link += "<div class='optilink'><img width='16' height='16' src='" + icon + "'>" + msg.link + "</div></a>";
-        headPanel.innerHTML = link;
-        sidePanel.appendChild(headPanel);
-
-        runMathJax(headPanel.querySelector(".title"));
-
-        if (panel.body) {
-            sidePanel.append(document.createElement("hr"));//body
-            panel.body.className += " optibody";
-            let codes = panel.body.querySelectorAll("code, pre");
-            codes.forEach(c => {
-                c.className += " prettyprint";
-            });
-            let pres = panel.body.querySelectorAll("pre");
-            pres.forEach(pre => {
-
-                var surround = document.createElement("div");
-                surround.style.position = "relative";
-                surround.innerHTML = pre.outerHTML;
-                surround.appendChild(createCopyButton(pre.innerText));
-
-                pre.parentNode.replaceChild(surround, pre);
-            });
-            sidePanel.appendChild(panel.body);
+        for (let r of results) {
+            const link = r.querySelector("a").href;
+            const found = Object.keys(Sites).find(site => save[site] && link.search(Sites[site].link) != -1)
+            if (found) {
+                port.postMessage({
+                    engine: engine,
+                    link: link,
+                    site: found
+                });
+                break;
+            }
         }
 
-        if (panel.foot) {
-            panel.foot.id = "output";
-            sidePanel.append(document.createElement("hr"));//foot
-            sidePanel.appendChild(panel.foot);
-        }
 
-        let links = sidePanel.querySelectorAll("a");
-        links.forEach(a => {
-            let ahref = a.getAttribute('href');
-            if (!ahref.startsWith("//") && !ahref.startsWith("http")) {
-                if (!ahref.startsWith("/")) {
-                    a.href = msg.link.replace(/\/[^\/]*$/, "") + "/" + ahref;
+        //set panel
+        function appendPanel(panel) {
+            let knowledgePanel = document.createElement("div");
+            knowledgePanel.className = "optisearchbox";
+            if (engine == Ecosia)
+                knowledgePanel.style.marginTop = "20px";
+            knowledgePanel.style.marginBottom = "20px";
+
+            knowledgePanel.appendChild(panel);
+            const rightColumn = document.querySelector(engines[engine].rightColumn)
+            if (!rightColumn)
+                console.warn('No right column detected')
+            else
+                rightColumn.appendChild(knowledgePanel);
+            return knowledgePanel;
+        }
+        port.onMessage.addListener(msg => {
+            var panel;
+            let icon;
+
+            // console.log(msg)
+
+            for (const site in Sites) {
+                if (Sites.hasOwnProperty(site)) {
+                    const p = Sites[site];
+                    if (msg.site == site) {
+                        icon = p.icon;
+                        panel = p.set(msg);
+                        if (site == 'stackexchange') {
+                            getChildrenTex(panel.body);
+                        }
+                        break;
+                    }
                 }
-                else
-                    a.href = host + ahref;
+            }
+            if (!panel)
+                return;
+
+
+            let host = msg.link.match("https?://[^/]+")[0];
+
+            var sidePanel = document.createElement("div");
+            sidePanel.className = PANEL_CLASS;
+
+            const watermark = document.createElement("div");
+            watermark.className = "watermark";
+            watermark.textContent = 'OptiSearch'
+            sidePanel.appendChild(watermark)
+
+            var headPanel = document.createElement("div");
+            headPanel.className = "optiheader";
+
+            msg.title = msg.title.replace(/<(\w*)>/g, '&lt;$1&gt;');
+
+            var link = "<a href='" + msg.link + "'><div class='title'>" + msg.title + "</div>";
+            link += "<div class='optilink'><img width='16' height='16' src='" + icon + "'>" + msg.link + "</div></a>";
+            headPanel.innerHTML = link;
+            sidePanel.appendChild(headPanel);
+
+            runMathJax(headPanel.querySelector(".title"));
+
+            if (panel.body) {
+                sidePanel.append(document.createElement("hr"));//body
+                panel.body.className += " optibody";
+                let codes = panel.body.querySelectorAll("code, pre");
+                codes.forEach(c => {
+                    c.className += " prettyprint";
+                });
+                let pres = panel.body.querySelectorAll("pre");
+                pres.forEach(pre => {
+
+                    var surround = document.createElement("div");
+                    surround.style.position = "relative";
+                    surround.innerHTML = pre.outerHTML;
+                    surround.appendChild(createCopyButton(pre.innerText));
+
+                    pre.parentNode.replaceChild(surround, pre);
+                });
+                sidePanel.appendChild(panel.body);
             }
 
+            if (panel.foot) {
+                panel.foot.id = "output";
+                sidePanel.append(document.createElement("hr"));//foot
+                sidePanel.appendChild(panel.foot);
+            }
+
+            let links = sidePanel.querySelectorAll("a");
+            links.forEach(a => {
+                let ahref = a.getAttribute('href');
+                if (!ahref.startsWith("//") && !ahref.startsWith("http")) {
+                    if (!ahref.startsWith("/")) {
+                        a.href = msg.link.replace(/\/[^\/]*$/, "") + "/" + ahref;
+                    }
+                    else
+                        a.href = host + ahref;
+                }
+
+            });
+
+
+            appendPanel(sidePanel);
+
+            PR.prettyPrint();
         });
 
-
-        appendPanel(sidePanel);
-
-        PR.prettyPrint();
-    });
-
+    })
 })
 
 
