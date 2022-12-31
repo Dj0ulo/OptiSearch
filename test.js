cff8ab2267ab8f4b35e309a205f42587ac08a78c
Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
2022-12-31 13:05:00 +0100

    Squashed commit of the following:
    commit 14e8c6174cb491f899b9ecb09f1a8b35fb370353
    Author: Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
    Date:   Sat Dec 31 13:01:22 2022 +0100
    
        [ADD] Test config and start
    
    commit 453000bed05295200d5dd11bdcb3f5c589d90252
    Author: Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
    Date:   Sat Dec 31 12:58:03 2022 +0100
    
        [IMP] ChatGPT : better formatting code and style
    
    commit 1cc9c8520fa3c02eef7bb4db4beceb4721068eeb
    Author: Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
    Date:   Mon Dec 26 23:48:41 2022 +0100
    
        [IMP] ChatGPT better styling + markdown formatting
    
    commit a481debc7127d41603e4dab63a82493a660179f2
    Author: Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
    Date:   Mon Dec 26 12:25:57 2022 +0100
    
        [IMP] ChatGPT prettify
    
    commit 5d9ada9584ae4556b52d1c835440f6a8437d3302
    Author: Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
    Date:   Mon Dec 26 00:04:20 2022 +0100
    
        [ADD] ChatGPT begin functionnality
    
    commit 3cbd454bae47ef1735d6bc5dde0f658c27e664d3
    Author: Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
    Date:   Sat Dec 24 17:31:15 2022 +0100
    
        [REF] Static class Context

---
+++ test.js
@@ -0,0 +1,144 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+/** @type {puppeteer.Page} */
+let page = null;
+let engines = null;
+
+let { ElementHandle, Page } = puppeteer;
+
+/**
+ * Set value on a select element
+ * @param {string} value
+ * @returns {Promise<Undefined>}
+ */
+ElementHandle.prototype.select = async function (value) {
+  await this._page.evaluateHandle((el, value) => {
+    const event = new Event("change", { bubbles: true });
+    event.simulated = true;
+    el.querySelector(`option[value="${value}"]`).selected = true;
+    el.dispatchEvent(event);
+  }, this, value);
+};
+
+/**
+ * Check if element is visible in the DOM
+ * @returns {Promise<Boolean>}
+ **/
+ElementHandle.prototype.isVisible = async function () {
+  return (await this.boundingBox() !== null);
+};
+
+/**
+ * Get element attribute
+ * @param {string} attr
+ * @returns {Promise<String>}
+ */
+ElementHandle.prototype.get = async function (attr, page) {
+  const handle = await page.evaluateHandle((el, attr) => el[attr], this, attr);
+  return await handle.jsonValue();
+};
+
+
+Page.prototype.assertEqual = function (actual, expected) {
+  assert.equal(actual, expected, `"${this.engineName}": {${actual}==${expected}}`);
+}
+Page.prototype.assertOk = function (value, message) {
+  assert.ok(value, `"${this.engineName}": ${message}`);
+}
+
+const EXTENSION_PATH = __dirname;
+
+const read = (file) => new Promise((resolve, reject) => {
+  fs.readFile(path.join(EXTENSION_PATH, file), { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(data);
+  });
+});
+
+
+
+/** @type {puppeteer.Browser} */
+let browser = null;
+
+const boot = async () => {
+  browser = await puppeteer.launch({
+    headless: false, // extension are allowed only in head-full mode
+    // executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+    defaultViewport: null,
+    devtools: true,
+    // slowMo: true,
+    args: [
+      `--disable-extensions-except=${EXTENSION_PATH}`,
+      `--load-extension=${EXTENSION_PATH}`,
+      '--enable-automation',
+      `--window-size=1920,1080`,
+    ]
+  });
+}
+
+/** @type {puppeteer.Page[]} */
+let pages = [];
+
+
+
+// Type of test
+describe('OptiPanel', function () {
+  this.timeout(2000000); // default is 2 seconds and that may not be enough to boot browsers and pages.
+  before(async () => {
+    await boot();
+    engines = JSON.parse(await read(`./src/engines.json`));
+  });
+  
+  // Part of the app
+  describe('Engines selector', async () => {
+    before(async () => {
+      pages = await Promise.all(Object.entries(engines).filter(([e,v])=>v.active).map(async ([e,v]) => {
+        const p = await browser.newPage();
+        p.logs = [];
+        p.engine = v;
+        p.engineName = e;
+        p.on('console', message => {
+          const text = message.text();
+          if (text.startsWith("%c[OptiSearch]")) {
+            p.logs.push({
+              type: message.type(),
+              text: text.substring("%c[OptiSearch] font-weight: bold; ".length),
+            });
+          }
+        });
+        return p;
+      }));
+
+      const helloPromises = Promise.all(pages.map(p => new Promise(resolve => {
+        p.on('console', () => p.logs.some((l) => l.text === "Hello !" && resolve()));
+      })));
+
+      await Promise.all(pages.map(p=> {
+        if(p.engineName === "DuckDuckGo")
+          return p.goto(`${p.engine.link}/?q=setinterval%20js`);
+        return p.goto(`${p.engine.link}/search?q=setinterval%20js`);
+      }));
+        
+      await helloPromises;
+    });
+
+    // Functionality
+    it('Search string', async () => {
+      await Promise.all(pages.map(async p => {
+        const el = await p.$(p.engine.searchBox);
+        p.assertEqual(await el.get("value", p), "setinterval js");
+      }));
+    })
+  });
+
+  // after(async () => await browser.close());
+});
+
+
+
